e448950b7bbde09ac1aab99818c588fa229298df
MAMADOU LAMARANA DIALLO <lamarana55@gmail.com>
2026-04-08 13:21:43 +0000

    update Dataloader

--- backend/src/main/java/gov/ravec/backend/config/DataInitializer.java
+++ backend/src/main/java/gov/ravec/backend/config/DataInitializer.java
@@ -16,6 +16,7 @@ import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * ============================================================
